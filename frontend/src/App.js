f5fcb5e572f35be6b60de85e3dbf57ed83efe6ab
Nachito <ignacio.lm1991@gmail.com>
2025-07-16 23:50:35 -0300

    primera carga

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+// Punto de entrada de la app React
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Sistema de Gestión Comercial</h1>
+    </div>
+  );
+}
+
+export default App;
